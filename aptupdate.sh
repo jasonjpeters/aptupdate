08ee02ce535f85fbf74cd67d0b980bdd1c527658
JasonJPeters <jasonjpeters@gmail.com>
2020-01-09 16:25:12 -0800

    init

---
+++ aptupdate.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+clear
+
+## sript name
+SCRIPT_NAME=aptupdate
+
+## environment variables
+START_TIME="$(date +%s)"
+
+SCRIPT_PTH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PTH")/
+SCRIPT_HSH=`md5sum ${SCRIPT_PTH} | awk '{ print $1 }'`
+
+CONFIGFILE=$SCRIPT_NAME.conf
+CONFIGFILE_DEFAULT=${SCRIPT_DIR}$SCRIPT_NAME.default.conf
+CONFIGFILE_ETC=/etc/$SCRIPT_NAME/$CONFIGFILE
+
+## formats string output
+function printTitle
+{
+    printf "\n"
+    echo "$1"
+    printf '%0.s-' $(seq 1 ${#1})
+    printf "\n"
+}
+
+## root check
+if ! [ $(id -u) = 0 ]; then
+    printTitle "Error: Must be run as root!"
+    printf "** sudo $SCRIPT_NAME \n\n"
+    exit
+fi
+
+## load configurations
+for CONFIGFILE_PATH in "$CONFIGFILE_DEFAULT" "$CONFIGFILE_ETC"
+do
+    if [ -f "$CONFIGFILE_PATH" ]; then
+        source "$CONFIGFILE_PATH"
+    fi
+done
+
+## upgrade system
+printTitle "Cleaning local cache"
+apt-get clean && echo "** Local package information cleaned"
+
+printTitle "Updating package information"
+apt-get update
+
+printTitle "Upgrading packages"
+apt-get dist-upgrade -y
+
+if [ "$RELEASE_UPGRADE" = 1 ] && [ "$SILENT" = "1" ]; then
+    printTitle "Release upgrade"
+    printf "** Upgrading system release version if available \n"
+    printf "** Using non-interactive mode \n"
+    do-release-upgrade -f DistUpgradeViewNonInteractive
+elif [ "$RELEASE_UPGRADE" = "1" ] && [ "$SILENT" = "0" ]; then
+    printTitle "Release upgrade"
+    printf "** Upgrading system release version if available \n"
+    printf '** Using interactive mode \n'
+    do-release-upgrade
+else
+    printTitle "Release upgrade"
+    printf "** Upgrading system release version skipped \n"
+fi
+
+printTitle "Package cleanup"
+apt-get autoremove -y
+
+echo "Update completed in $((($(date +%s)-$START_TIME)/60)) min"
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+clear
+
+## script name
+SCRIPT_NAME=aptupdate
+
+## environment variables
+WORKING_DIR="$(pwd)"
+INSTALL_DIR="/usr/lib/$SCRIPT_NAME/"
+
+## create conifg directory
+if [ ! -d "/etc/aptupdate" ]; then
+    mkdir -p "/etc/$SCRIPT_NAME"
+fi
+
+## install pre-requisites
+apt update
+apt install git -y
+
+## install/update
+if [ -d "$INSTALL_DIR" ]; then
+    echo "Installing APTUpdate..."
+    mkdir -p "$INSTALL_DIR"
+    cd "$INSTALL_DIR"
+    git clone git@github.com:jasonjpeters/${SCRIPT_NAME}.git ./
+else
+    echo "Updating APTUpdate..."
+fi
+
+## fetch & pull code
+cd $INSTALL_DIR
+git pull
+
+## symlink (global command)
+if [ ! -e "/usr/bin/${SCRIPT_NAME}" ]l then
+    ln -s ${INSTALL_DIR}$SCRIPT_NAME.sh /usr/bin/${SCRIPT_NAME}
+fi
+
+## restore working directory
+cd $WORKING_DIR
